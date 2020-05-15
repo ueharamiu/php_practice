54209dfc7ffa7598fd8a90cec31fec12b0df3e3a
ueharamiu <ueharamiu66@gmail.com>
2020-05-15 15:23:30 +0900

    2020.5.15 style.css 記述

--- index.php
+++ index.php
@@ -3,28 +3,30 @@
   <head>
     <meta charset="utf-8">
     <title>Calendar</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-      <table class="table table-boedered"> 
-          <tr>
-              <th>日</th>
-              <th>月</th>
-              <th>火</th>
-              <th>水</th>
-              <th>木</th>
-              <th>金</th>
-              <th>土</th> 
-          </tr>
-          <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-              <td>7</td>
-          </tr>
-      </table>
-    </body>
+    <div class="container">
+        <table class="table table-boedered"> 
+            <tr>
+                <th>日</th>
+                <th>月</th>
+                <th>火</th>
+                <th>水</th>
+                <th>木</th>
+                <th>金</th>
+                <th>土</th> 
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+            </tr>
+        </table>
+    </div>
+  </body>
   </html>
