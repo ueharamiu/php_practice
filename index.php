13982e2bb4577a02880a935a72c127b48f5ee0cf
ueharamiu <ueharamiu66@gmail.com>
2020-05-23 21:01:50 +0900

    2020.5.23 作業

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
-$week = array( "日", "月", "火", "水", "木", "金", "土");
+$weeks = array( "日", "月", "火", "水", "木", "金", "土");
     
 $days = array( 1, 2, 3, 4, 5, 6, 7);
-    for ($day = 0; $day <= 7; $day++)
+   
 ?>
 
 
@@ -18,22 +18,14 @@ $days = array( 1, 2, 3, 4, 5, 6, 7);
     <div class="container">  
         <table class="table table-boedered"> 
             <tr>
-                <th><?php echo $week[0]; ?></th>
-                <th><?php echo $week[1]; ?></th>
-                <th><?php echo $week[2]; ?></th>
-                <th><?php echo $week[3]; ?></th>
-                <th><?php echo $week[4]; ?></th>
-                <th><?php echo $week[5]; ?></th>
-                <th><?php echo $week[6]; ?></th> 
+            <th><?php for ($week = 0; $week <= 7; $week++) {
+                 echo $weeks[$week]; } ?></th>
+                 <?php echo '<th>'; ?>
             </tr>
             <tr>
-                <td><?php echo $days[0]; ?></td>
-                <td><?php echo $days[1]; ?></td>
-                <td><?php echo $days[2]; ?></td>
-                <td><?php echo $days[3]; ?></td>
-                <td><?php echo $days[4]; ?></td>
-                <td><?php echo $days[5]; ?></td>
-                <td><?php echo $days[6]; ?></td>
+                <td><?php  for ($day = 0; $day <= 7; $day++) {
+      echo $days[$day];
+    } ?></td>
             </tr>
         </table>
     </div>
