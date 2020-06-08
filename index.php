c0f1a52797ae28d5c7fa680fdc12f1733eab0601
ueharamiu <ueharamiu66@gmail.com>
2020-06-08 17:28:33 +0900

    2020.6.8 カレンダー表示完成

--- index.php
+++ index.php
@@ -15,86 +15,21 @@ $days = array();
   <body>
     <div class="container">  
         <table class="table table-boedered"> 
-            <!--  
-            <tr>
-              <?php for ($week = 0; $week < 7; $week++) { ?>
-              <th><?php echo $weeks[$week]; } ?></th> 
-            </tr>
-
-           
-            <tr>
-              <?php for ($day = 1; $day < 32; $day++) { 
-               echo "<td>"; 
-               echo $day; 
-               } ?>  
+           <tr>
+            <?php
+            for ($week = 0; $week < 7; $week++) {
+              echo "<th>" . $weeks[$week] . "</th>"; 
+            }
+            ?>
            </tr>
-           -->
-           
-            
            <?php
-           for($tr = 1; $tr < 6; $tr++) {
-            
-            
-             for($td = 1; $td < 8; $td++) {
               for ($day = 1; $day < 32; $day++) {
                 echo "<td>" . $day . "</td>";
-                if ( $day == 8 || $day || 16 || $day == 23) {
-                  echo "</tr><tr>";
-              }}}
-              
-             echo "</tr>";
-           } ?>
-           
-           
-             <!--  
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-              <td>7</td>
-            </tr>
-            <tr>
-              <td>8</td>
-              <td>9</td>
-              <td>10</td>
-              <td>11</td>
-              <td>12</td>
-              <td>13</td>
-              <td>14</td>
-            </tr>
-            <tr>
-              <td>15</td>
-              <td>16</td>
-              <td>17</td>
-              <td>18</td>
-              <td>19</td>
-              <td>20</td>
-              <td>21</td>
-            </tr>
-            <tr>
-              <td>22</td>
-              <td>23</td>
-              <td>24</td>
-              <td>25</td>
-              <td>26</td>
-              <td>27</td>
-              <td>28</td>
-            </tr>
-            <tr>
-              <td>29</td>
-              <td>30</td>
-              <td>31</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            -->
-            
-              
+                  if ( $day % 7 == 0) {
+                    echo "</tr><tr>";
+                  }
+              }
+              ?>     
         </table>
     </div>
   </body>
