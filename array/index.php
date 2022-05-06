404a22dfffef0e91f00f637c4800b61baed671a3
ueharamiu <ueharamiu66@gmail.com>
2022-05-06 17:47:36 +0900

    配列に要素を追加した場合の記述を追加

--- array/index.php
+++ array/index.php
@@ -16,6 +16,24 @@
 	];
 ?>
 
+<!-- 配列の要素の追加 -->
+<?php
+	$imgarray = [
+		'img/dog3.jpg',
+		'600'
+	];
+	array_splice( $imgarray, 2, 0, '440.53' ); // 1つ目は要素を追加する配列、2つ目は追加する位置、3つ目は置き換える要素数、4つ目が追加する要素
+?>
+
+<!-- 連想配列の要素の追加 -->
+<?php
+	$imagearray = [
+		'path' => 'img/dog4.jpg'
+	];
+	$imagearray[ 'width' ] = 600;
+	$imagearray[ 'height' ] = 415.71;
+?>
+
 <!DOCTYPE html>
 	<html lang="ja">
 	<head>
@@ -44,6 +62,38 @@
 						echo $image['height']; 
 					?>" 
 				alt="愛犬の画像2">
+		</div>
+		<div>
+			<h2>配列の要素を追加して出力</h2>
+			<img src="
+				<?php 
+					echo $imgarray[0] 
+				?>" 
+				width="
+					<?php 
+						echo $imgarray[1] 
+					?>" 
+				height="
+					<?php 
+						echo $imgarray[2] 
+					?>" 
+				alt="愛犬の画像3">
+		</div>
+		<div>
+			<h2>連想配列の要素を追加して出力</h2>
+			<img src="
+				<?php 
+					echo $imagearray['path'] 
+				?>" 
+				width="
+					<?php 
+						echo $imagearray['width']
+					?>"
+				height="
+					<?php 
+						echo $imagearray['height']
+					?>"	
+				alt="愛犬の画像4">
 		</div>
 	</body>
 </html>
